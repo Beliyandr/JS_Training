ed9a2ef3df694c67763d684add4ebe33aa308403
beliyandr <beliyandr@gmail.com>
2021-10-14 13:30:36 +0300

    trains

--- FreeCodeCamp/01.js
+++ FreeCodeCamp/01.js
@@ -1,1 +1,15 @@
-console.log('123')
+//1 Understanding Case Sensitivity in Variables
+
+// Variable declarations
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
+
+// // Variable assignments
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
+
+// //2 Add Two Numbers with JavaScript
+
+// var sum = 20 + 0;
