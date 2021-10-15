ff09bdc3496aa1676db943887f966ac830684059
beliyandr <beliyandr@gmail.com>
2021-10-15 20:58:20 +0300

    freecode_wars

--- FreeCodeCamp/01.js
+++ FreeCodeCamp/01.js
@@ -13,3 +13,34 @@
 // //2 Add Two Numbers with JavaScript
 
 // var sum = 20 + 0;
+
+// var myStr = 'I am a "double quoted" string inside "double quotes".'; // Change this line
+
+// var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+// var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+// ES6
+
+// 1.
+function checkScope() {
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
+
+// 2.Declare a Read-Only Variable with the const Keyword
+function printManyTimes(str) {
+  // Only change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for (let i = 0; i < str.length; i += 2) {
+    console.log(SENTENCE);
+  }
+
+  // Only change code above this line
+}
+printManyTimes("freeCodeCamp");
