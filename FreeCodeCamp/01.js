5fdb8458fcad192286876a118d636fa4250c4d27
beliyandr <beliyandr@gmail.com>
2021-10-16 19:43:24 +0300

    freecodecamp

--- FreeCodeCamp/01.js
+++ FreeCodeCamp/01.js
@@ -44,3 +44,37 @@ function printManyTimes(str) {
   // Only change code above this line
 }
 printManyTimes("freeCodeCamp");
+
+// 3.Mutate an Array Declared with const
+
+const s = [5, 7, 2];
+function editInPlace() {
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+  // Only change code below this line
+
+  // Using s = [2, 5, 7] would be invalid
+
+  // Only change code above this line
+}
+editInPlace();
+
+// 4.Prevent Object Mutation
+
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14,
+  };
+  // Only change code below this line
+  Object.freeze(MATH_CONSTANTS);
+
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch (ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
