26ddf36d895a0c7f2dac680446911976cefb6afe
beliyandr <beliyandr@gmail.com>
2021-10-21 20:38:06 +0300

    cde

--- FreeCodeCamp/01.js
+++ FreeCodeCamp/01.js
@@ -80,17 +80,16 @@ function freezeObj() {
 }
 const PI = freezeObj();
 
-
 //Concatenating Strings with the Plus Equals Operator
-var myStr = 'This is the first sentence. ';
-myStr += 'This is the second sentence.';
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
 
 //Constructing Strings with Variables
-var myName = 'Andrey';
-var myStr = 'My name is' + myName + 'and I am well!'
+var myName = "Andrey";
+var myStr = "My name is" + myName + "and I am well!";
 
 //Appending Variables to Strings
-var someAdjective = 'JS HTML CSS';
+var someAdjective = "JS HTML CSS";
 var myStr = "Learning to code is ";
 myStr += someAdjective;
 
@@ -102,7 +101,6 @@ var lastName = "Lovelace";
 lastNameLength = lastName.length;
 lastNameLength = 8;
 
-
 //Word Blanks
 
 var myNoun = "dog";
@@ -111,9 +109,9 @@ var myVerb = "ran";
 var myAdverb = "quickly";
 
 // Only change code below this line
-var wordBlanks = myNoun + ',' + myVerb + ',' + myAdjective + ',' + 'or' +  ' ' + myAdverb;
+var wordBlanks =
+  myNoun + "," + myVerb + "," + myAdjective + "," + "or" + " " + myAdverb;
 console.log(wordBlanks);
-
 
 //Store Multiple Values in one Variable using JavaScript Arrays
 
@@ -125,18 +123,19 @@ console.log(wordBlanks);
 // Passed
 // The second item in myArray should be a number.
 
-var myArray = ['sc', 3];
+var myArray = ["sc", 3];
 
 //!Nest one Array within Another Array
 // myArray should have at least one array nested within another array.
-var myArray = [['123',324], ['sam',555]];
-
+var myArray = [
+  ["123", 324],
+  ["sam", 555],
+];
 
 //!Access Array Data with Indexes
-var myArray = [50,60,70];
+var myArray = [50, 60, 70];
 var myData = myArray[0];
-console.log(myData)
-
+console.log(myData);
 
 //!Manipulate Arrays With push()
 // var arr1 = [1,2,3];
@@ -145,8 +144,11 @@ console.log(myData)
 // var arr2 = ["Stimpson", "J", "cat"];
 // arr2.push(["happy", "joy"]);
 // arr1 now has the value [1, 2, 3, 4] and arr2 has the value ["Stimpson", "J", "cat", ["happy", "joy"]]
-var myArray = [["John", 23], ["cat", 2]];
-myArray.push(["dog", 3])
+var myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
+myArray.push(["dog", 3]);
 
 //!Manipulate Arrays With pop()
 // Another way to change the data in an array is with the .pop() function.
@@ -164,7 +166,10 @@ myArray.push(["dog", 3])
 // Use the .pop() function to remove the last item from myArray, assigning the popped off value to removedFromMyArray.
 
 // Setup
-var myArray = [["John", 23], ["cat", 2]];
+var myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
 
 // Only change code below this line
 var removedFromMyArray = myArray.pop();
@@ -183,19 +188,41 @@ var removedFromMyArray = myArray.pop();
 
 // Add ["Paul",35] to the beginning of the myArray variable using unshift().
 
-var myArray = [["John", 23], ["dog", 3]];
+var myArray = [
+  ["John", 23],
+  ["dog", 3],
+];
 myArray.shift();
-myArray.unshift(['Paul',35]);
-
+myArray.unshift(["Paul", 35]);
 
 //!Shopping List
-var myList = [['choco', 15], ['cofe', 20],['milk', 11],['tea', 33],['water', 1] ];
-
+var myList = [
+  ["choco", 15],
+  ["cofe", 20],
+  ["milk", 11],
+  ["tea", 33],
+  ["water", 1],
+];
 
 // Write Reusable JavaScript with Functions
 
-function reusableFunction (){
-  console.log('Hi World')
+function reusableFunction() {
+  console.log("Hi World");
 }
 
-reusableFunction ();
+reusableFunction();
+
+//Passing Values to Functions with Arguments
+// Parameters are variables that act as placeholders for the values that are to be input to a function when it is called. When a function is defined, it is typically defined along with one or more parameters. The actual values that are input (or "passed") into a function when it is called are known as arguments.
+
+// Here is a function with two parameters, param1 and param2:
+
+// function testFun(param1, param2) {
+//   console.log(param1, param2);
+// }
+
+function functionWithArgs(par1, par2) {
+  console.log(par1 + par2);
+}
+
+functionWithArgs(1, 2);
