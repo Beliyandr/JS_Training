46d2f58bc8bef202784284214007ab004d4f0e29
beliyandr <beliyandr@gmail.com>
2021-10-30 21:53:02 +0300

    ode

--- FreeCodeCamp/01.js
+++ FreeCodeCamp/01.js
@@ -284,3 +284,26 @@ myLocalScope();
 // Run and check the console
 // myVar is not defined outside of myLocalScope
 console.log("outside myLocalScope", myVar);
+
+//Global vs. Local Scope in Functions
+// In this example:
+
+// var someVar = "Hat";
+// function myFun() {
+//   var someVar = "Head";
+//   return someVar;
+// }
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+
+  var outerWear = "sweater";
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
