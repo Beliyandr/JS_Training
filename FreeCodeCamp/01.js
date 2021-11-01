5e1aac32f8a976a614eb8917fd8ef0049eb0f499
beliyandr <beliyandr@gmail.com>
2021-11-01 18:47:03 +0200

    code

--- FreeCodeCamp/01.js
+++ FreeCodeCamp/01.js
@@ -308,7 +308,6 @@ function myOutfit() {
 
 myOutfit();
 
-
 //! Understanding Undefined Value returned from a Function
 // A function can include the return statement but it does not have to. In the case that the function doesn't have a return statement, when you call it, the function processes the inner code but the returned value is undefined.
 
@@ -325,7 +324,6 @@ myOutfit();
 
 // Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.
 
-
 // Setup
 let sum = 0;
 
@@ -334,22 +332,19 @@ function addThree() {
 }
 
 function addFive() {
-  sum = sum + 5
+  sum = sum + 5;
 }
 
 // Only change code below this line
-
 
 // Only change code above this line
 
 addThree();
 addFive();
 
-
 // ----------------------------------------------
 
 // Assignment with a Returned Value
-
 
 //! Stand in Line
 // In Computer Science a queue is an abstract Data Structure where items are kept in order. New items can be added at the back of the queue and old items are taken off from the front of the queue.
@@ -362,7 +357,7 @@ addFive();
 
 function nextInLine(arr, item) {
   // Only change code below this line
-  arr.push(item)
+  arr.push(item);
 
   return arr.shift();
   // Only change code above this line
@@ -376,4 +371,37 @@ console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
 
+//Use Conditional Logic with If Statements
+// if statements are used to make decisions in code. The keyword if tells JavaScript to execute the code in the curly braces under certain conditions, defined in the parentheses. These conditions are known as Boolean conditions and they may only be true or false.
 
+// When the condition evaluates to true, the program executes the statement inside the curly braces. When the Boolean condition evaluates to false, the statement inside the curly braces will not execute.
+
+// Pseudocode
+
+// if (condition is true) {
+//   statement is executed
+// }
+// Example
+
+// function test (myCondition) {
+//   if (myCondition) {
+//     return "It was true";
+//   }
+//   return "It was false";
+// }
+
+// test(true);
+// test(false);
+// test(true) returns the string It was true, and test(false) returns the string It was false.
+
+// When test is called with a value of true, the if statement evaluates myCondition to see if it is true or not. Since it is true, the function returns It was true. When we call test with a value of false, myCondition is not true and the statement in the curly braces is not executed and the function returns It was false.
+
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+  if (wasThatTrue) {
+    return "Yes, that was true";
+  }
+  return "No, that was false";
+
+  // Only change code above this line
+}
