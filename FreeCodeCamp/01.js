77b7887c08715922a16f9c7571a0b81148d24c18
beliyandr <beliyandr@gmail.com>
2021-11-07 16:18:51 +0200

    code

--- FreeCodeCamp/01.js
+++ FreeCodeCamp/01.js
@@ -940,3 +940,334 @@ function caseInSwitch(val) {
 }
 
 caseInSwitch(1);
+
+
+// Adding a Default Option in Switch Statements
+// In a switch statement you may not be able to specify all possible values as case statements. Instead, you can add the default statement which will be executed if no matching case statements are found. Think of it like the final else statement in an if/else chain.
+
+// A default statement should be the last case.
+
+// switch (num) {
+//   case value1:
+//     statement1;
+//     break;
+//   case value2:
+//     statement2;
+//     break;
+// ...
+//   default:
+//     defaultStatement;
+//     break;
+// }
+// Write a switch statement to set answer for the following conditions:
+// a - apple
+// b - bird
+// c - cat
+// default - stuff
+
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 'a':
+      answer = 'apple';
+      break;
+
+    case 'b':
+      answer = 'bird';
+      break;
+
+    case 'c':
+      answer = 'cat';
+      break;
+
+    default:
+      answer = 'stuff';
+      break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+
+
+// Multiple Identical Options in Switch Statements
+// If the break statement is omitted from a switch statement's case, the following case statement(s) are executed until a break is encountered. If you have multiple inputs with the same output, you can represent them in a switch statement like this:
+
+// let result = "";
+// switch(val) {
+//   case 1:
+//   case 2:
+//   case 3:
+//     result = "1, 2, or 3";
+//     break;
+//   case 4:
+//     result = "4 alone";
+// }
+// Cases for 1, 2, and 3 will all produce the same result.
+
+// Write a switch statement to set answer for the following ranges:
+// 1-3 - Low
+// 4-6 - Mid
+// 7-9 - High
+
+// Note: You will need to have a case statement for each number in the range.
+
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = 'Low';
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = 'Mid';
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = 'High';
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+
+
+// Replacing If Else Chains with Switch
+// If you have many options to choose from, a switch statement can be easier to write than many chained if/else if statements. The following:
+
+// if (val === 1) {
+//   answer = "a";
+// } else if (val === 2) {
+//   answer = "b";
+// } else {
+//   answer = "c";
+// }
+// can be replaced with:
+
+// switch(val) {
+//   case 1:
+//     answer = "a";
+//     break;
+//   case 2:
+//     answer = "b";
+//     break;
+//   default:
+//     answer = "c";
+// }
+// Change the chained if/else if statements into a switch statement.
+
+// Replacing If Else Chains with Switch
+// If you have many options to choose from, a switch statement can be easier to write than many chained if/else if statements. The following:
+
+// if (val === 1) {
+//   answer = "a";
+// } else if (val === 2) {
+//   answer = "b";
+// } else {
+//   answer = "c";
+// }
+// can be replaced with:
+
+// switch(val) {
+//   case 1:
+//     answer = "a";
+//     break;
+//   case 2:
+//     answer = "b";
+//     break;
+//   default:
+//     answer = "c";
+// }
+// Change the chained if/else if statements into a switch statement.
+
+
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+
+
+
+  switch (val) {
+    case 'bob':
+      answer = 'Marley';
+      break;
+    case 42:
+      answer = 'The Answer';
+      break;
+    case 1:
+      answer = 'There is no #1';
+      break;
+    case 99:
+      answer = 'Missed me by this much!';
+      break;
+    case 7:
+      answer = 'Ate Nine';
+      break;
+    default:
+      answer = '';
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+
+// Returning Boolean Values from Functions
+// You may recall from Comparison with the Equality Operator that all comparison operators return a boolean true or false value.
+
+// Sometimes people use an if/else statement to do a comparison, like this:
+
+// function isEqual(a, b) {
+//   if (a === b) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// But there's a better way to do this. Since === returns true or false, we can return the result of the comparison:
+
+// function isEqual(a, b) {
+//   return a === b;
+// }
+// Fix the function isLess to remove the if/else statements.
+function isLess(a, b) {
+  // Only change code below this line
+  // if (a < b) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+  return (a < b);
+  // Only change code above this line
+}
+
+isLess(10, 15);
+
+
+// Return Early Pattern for Functions
+// When a return statement is reached, the execution of the current function stops and control returns to the calling location.
+
+// Example
+
+// function myFun() {
+//   console.log("Hello");
+//   return "World";
+//   console.log("byebye")
+// }
+// myFun();
+// The above will display the string Hello in the console, and return the string World. The string byebye will never display in the console, because the function exits at the return statement.
+
+// Modify the function abTest so that if a or b are less than 0 the function will immediately exit with a value of undefined.
+
+// Hint
+// Remember that undefined is a keyword, not a string.
+
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+  if (a < 0 || b < 0) {
+    return undefined
+  }
+
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2, 2);
+
+
+
+
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count--;
+      break;
+  }
+
+  let holdbet = 'Hold';
+
+  if (count > 0) {
+    holdbet = 'Bet';
+  }
+
+  return count + ' ' + holdbet;
+  // Only change code above this line
+}
+
+console.log(cc(2));
+console.log(cc(3));
+console.log(cc(4));
+console.log(cc('K'));
+console.log(cc('A'));
+
+
+// Build JavaScript Objects
+// You may have heard the term object before.
+
+// Objects are similar to arrays, except that instead of using indexes to access and modify their data, you access the data in objects through what are called properties.
+
+// Objects are useful for storing data in a structured way, and can represent real world objects, like a cat.
+
+// Here's a sample cat object:
+
+// const cat = {
+//   "name": "Whiskers",
+//   "legs": 4,
+//   "tails": 1,
+//   "enemies": ["Water", "Dogs"]
+// };
+// In this example, all the properties are stored as strings, such as name, legs, and tails. However, you can also use numbers as properties. You can even omit the quotes for single-word string properties, as follows:
+
+// const anotherObject = {
+//   make: "Ford",
+//   5: "five",
+//   "model": "focus"
+// };
+// However, if your object has any non-string properties, JavaScript will automatically typecast them as strings.
+
+// Make an object that represents a dog called myDog which contains the properties name (a string), legs, tails and friends.
+
+// You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers, and friends is an array.
+
+const myDog = {
+  // Only change code below this line
+  name: 'myDog',
+  legs: 4,
+  tails: 5,
+  friends: ['Bob', 'Lol']
+
+  // Only change code above this line
+};
