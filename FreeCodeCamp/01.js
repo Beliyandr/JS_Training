f02a34afbe581c897be0d0e1bc954c800671b530
beliyandr <beliyandr@gmail.com>
2021-11-11 16:46:12 +0200

    code

--- FreeCodeCamp/01.js
+++ FreeCodeCamp/01.js
@@ -1902,3 +1902,91 @@ function updateRecords(records, id, prop, value) {
 
   return records;
 }
+
+
+//!Iterate with JavaScript While Loops
+// You can run the same code multiple times by using a loop.
+
+// The first type of loop we will learn is called a while loop because it runs while a specified condition is true and stops once that condition is no longer true.
+
+// const ourArray = [];
+// let i = 0;
+
+// while (i < 5) {
+//   ourArray.push(i);
+//   i++;
+// }
+// In the code example above, the while loop will execute 5 times and append the numbers 0 through 4 to ourArray.
+
+// Let's try getting a while loop to work by pushing values to an array.
+
+// Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
+
+
+// Setup
+const myArray = [];
+let i = 5;
+while(i>=0){
+  myArray.push(i)
+
+  i--
+}
+// Only change code below this line
+
+
+//Iterate with JavaScript For Loops
+// You can run the same code multiple times by using a loop.
+
+// The most common type of JavaScript loop is called a for loop because it runs for a specific number of times.
+
+// For loops are declared with three optional expressions separated by semicolons:
+
+// for (a; b; c), where a is the initialization statement, b is the condition statement, and c is the final expression.
+
+// The initialization statement is executed one time only before the loop starts. It is typically used to define and setup your loop variable.
+
+// The condition statement is evaluated at the beginning of every loop iteration and will continue as long as it evaluates to true. When the condition is false at the start of the iteration, the loop will stop executing. This means if the condition starts as false, your loop will never execute.
+
+// The final expression is executed at the end of each loop iteration, prior to the next condition check and is usually used to increment or decrement your loop counter.
+
+// In the following example we initialize with i = 0 and iterate while our condition i < 5 is true. We'll increment i by 1 in each loop iteration with i++ as our final expression.
+
+// const ourArray = [];
+
+// for (let i = 0; i < 5; i++) {
+//   ourArray.push(i);
+// }
+// ourArray will now have the value [0, 1, 2, 3, 4].
+
+// Use a for loop to push the values 1 through 5 onto myArray.
+
+// Setup
+// const myArray = [];
+// for (let i = 1 ; i<=5 ; i++){
+//   myArray.push(i)
+// }
+// // Only change code below this line
+
+
+// //Iterate Odd Numbers With a For Loop
+// For loops don't have to iterate one at a time. By changing our final-expression, we can count by even numbers.
+
+// We'll start at i = 0 and loop while i < 10. We'll increment i by 2 each loop with i += 2.
+
+// const ourArray = [];
+
+// for (let i = 0; i < 10; i += 2) {
+//   ourArray.push(i);
+// }
+// ourArray will now contain [0, 2, 4, 6, 8]. Let's change our initialization so we can count by odd numbers.
+
+// Push the odd numbers from 1 through 9 to myArray using a for loop.
+
+
+
+// Setup
+const myArray = [];
+for(let i=1; i< 10; i+=2){
+  myArray.push(i)
+}
+// Only change code below this line
