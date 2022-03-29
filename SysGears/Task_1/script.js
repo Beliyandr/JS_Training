7a62189651908fa1252da004f9aebfa511db6386
beliyandr <beliyandr@gmail.com>
2022-03-29 16:25:03 +0300

    task_1_completed

--- SysGears/Task_1/script.js
+++ SysGears/Task_1/script.js
@@ -1,29 +1,9 @@
-/*метры (m),
-сантиметры (cm),
-дюймы (in) Inches
-футы (ft),
-
-миллиметров (mm)
-ярдов (yd)
-  километров (km)
-*/
-
 const obj = {
   distance: { unit: "m", value: 0.5 },
   convert_to: "ft",
 };
 
-const obj2 = {
-  distance: { unit: "m", value: 0.5 },
-  convert_to: "cm",
-};
-
-const obj3 = {
-  distance: { unit: "in", value: 0.5 },
-  convert_to: "in",
-};
-
-const converted = (obj) => {
+function converted(obj) {
   let unit = obj.distance.unit; // начальная единица измерения
   let value = obj.distance.value; // значение
   let convTo = obj.convert_to; // конечная единица измерения
@@ -34,22 +14,52 @@ const converted = (obj) => {
   }
 
   switch (unit) {
+    case "mm": //Преобразование из mm в другие измерения
+      return mmTo();
+    case "cm": //Преобразование из см в другие измерения
+      return cmTo();
     case "m": // Преобразование из метров в другие измерения
       return mTo();
+    case "km": //Преобразование из километров в другие измерения
+      return kmTo();
     case "ft": // Преобразовать из футов в другие измерения
       return ftTo();
 
     case "in": //Преобразование из дюймов в другие измерения
       return inTo();
 
-    case "cm": //Преобразование из см в другие измерения
-      return cmTo();
-    case "km": //Преобразование из километров в другие измерения
-      return kmTo();
     case "yd": //Преобразовать с ярдов в другие измерения
       return ydTo();
     default:
       return null;
+  }
+
+  function mmTo() {
+    if (convTo === "m") {
+      convValue = (value / 1000).toFixed(2);
+      return { unit: convTo, value: convValue };
+    }
+    if (convTo === "in") {
+      convValue = (value * 0.03937).toFixed(2);
+      return { unit: convTo, value: convValue };
+    }
+    if (convTo === "ft") {
+      convValue = (value * 0.0032808).toFixed(2);
+      return { unit: convTo, value: convValue };
+    }
+    if (convTo === "cm") {
+      convValue = (value / 10).toFixed(2);
+      return { unit: convTo, value: convValue };
+    }
+
+    if (convTo === "km") {
+      convValue = (value / 1000000).toFixed(2);
+      return { unit: convTo, value: convValue };
+    }
+    if (convTo === "yd") {
+      convValue = (value * 0.0010936).toFixed(2);
+      return { unit: convTo, value: convValue };
+    }
   }
 
   function mTo() {
@@ -216,14 +226,13 @@ const converted = (obj) => {
       return { unit: convTo, value: convValue };
     }
   }
-};
+}
 
 // console.log(JSON.stringify(converted(obj)));
-// console.log(converted(obj2));
-// console.log(converted(obj3));
+// console.log(converted(obj));
 
 //*******************************************************
-
+// Получение DOM-элементов
 let refs = {
   form: document.querySelector(".form"),
 
@@ -237,36 +246,76 @@ let refs = {
 };
 
 refs.form.addEventListener("click", (event) => {
-  let convInValue = "";
+  handleClick();
+});
+
+function handleClick() {
+  let convInValue = refs.formInputIn.value;
   let convInVariable = refs.formSelectIn.value;
-  let convToValue = "";
+  let convToValue = refs.formInputTo.value;
   let convToVariable = refs.formSelectTo.value;
 
+  // Работа с исходной величиной
   refs.formSelectIn.addEventListener("change", (e) => {
     convInVariable = e.target.value;
+    convertedInForm();
   });
 
   refs.formInputIn.addEventListener("input", (e) => {
     convInValue = e.target.value;
+    convertedInForm();
+  });
 
+  //*********************************
+
+  refs.formSelectTo.addEventListener("change", (e) => {
+    convToVariable = e.target.value;
+    convertedInForm();
+  });
+
+  function convertedInForm() {
+    //процес конвертации внутри формы (с выводом результата в HTML-разметку)
     let obj = {
       distance: { unit: convInVariable, value: convInValue },
       convert_to: convToVariable,
     };
 
     convToValue = converted(obj);
-    convertedInForm(convToValue, refs.formInputTo);
-  });
-
-  refs.formSelectTo.addEventListener("change", (e) => {
-    convToVariable = e.target.value;
-  });
-
-  function convertedInForm(convValueObj, InputTo) {
     if (convInValue === "") {
-      InputTo.value = "";
+      refs.formInputTo.value = "";
     } else {
-      InputTo.value = convToValue.value;
+      refs.formInputTo.value = convToValue.value;
     }
   }
-});
+}
+
+const selectedObj = [
+  { unit: "m", name: "Метры" },
+  { unit: "cm", name: "Сантиметры" },
+  { unit: "mm", name: "Миллиметры" },
+  { unit: "km", name: "Киллометры" },
+  { unit: "in", name: "Дюймы" },
+  { unit: "ft", name: "Футы" },
+  { unit: "yd", name: "Ярды" },
+];
+
+function addSelected(arrayOptions) {
+  arrayOptions.forEach((item) => {
+    let text = `<option value=${item.unit}>${item.name}</option>`;
+    let textActive = `<option value=${item.unit} selected>${item.name}</option>`;
+    if (item.unit === "m") {
+      refs.formSelectIn.insertAdjacentHTML("beforeend", textActive);
+      refs.formSelectTo.insertAdjacentHTML("beforeend", text);
+      return;
+    }
+    if (item.unit === "yd") {
+      refs.formSelectIn.insertAdjacentHTML("beforeend", text);
+      refs.formSelectTo.insertAdjacentHTML("beforeend", textActive);
+      return;
+    }
+    refs.formSelectIn.insertAdjacentHTML("beforeend", text);
+    refs.formSelectTo.insertAdjacentHTML("beforeend", text);
+  });
+}
+
+addSelected(selectedObj);
